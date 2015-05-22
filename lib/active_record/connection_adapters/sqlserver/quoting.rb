8abb8518111a351b77f3db8154e15bd726f01536
export TERM=vt100 <vagrant@ic-dev-box>
2015-05-22 14:22:07 +0000

    I replaced the letter N to a more suitable location.

--- lib/active_record/connection_adapters/sqlserver/quoting.rb
+++ lib/active_record/connection_adapters/sqlserver/quoting.rb
@@ -3,7 +3,7 @@ module ActiveRecord
     module Sqlserver
       module Quoting
         QUOTED_TRUE, QUOTED_FALSE = '1', '0'
-        QUOTED_STRING_PREFIX = 'N'
+        QUOTED_STRING_PREFIX = ''
 
         def quote(value, column = nil)
           case value
